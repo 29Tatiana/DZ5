9647624a40d8dd01fe46e423fd57a2eaa2e268ac
29Tatiana <amiravob@mail.ru>
2022-09-19 14:42:11 +0300

    задача36r

--- задача36/Program.cs
+++ задача36/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+CreateArray(numbers);
+PrintArray(numbers);
+int sum = 0;
+
+for (int j = 1; j < numbers.Length; j += 2)
+{
+    sum = sum + numbers[j];
+}
+Console.WriteLine($"сумма элементов на нечётных позициях - {sum}");
+
+
+void CreateArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(-100, 100);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    foreach (int number in numbers)
+    {
+        Console.Write($"{number}, ");
+    }
+}
