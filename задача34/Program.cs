95d47ef50efbf0eb9f69dc18e6da8a4bd6617583
29Tatiana <amiravob@mail.ru>
2022-09-19 14:02:05 +0300

    задача34r

--- задача34/Program.cs
+++ задача34/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+CreateArray(numbers);
+PrintArray(numbers);
+int count = 0;
+
+for (int j = 0; j < numbers.Length; j++)
+{
+    if (numbers[j] % 2 == 0)
+        count++;
+}
+Console.WriteLine($"количество четных - {count}");
+
+
+void CreateArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100, 1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    foreach (int number in numbers)
+    {
+        Console.Write($"{number}, ");
+    }
+}
